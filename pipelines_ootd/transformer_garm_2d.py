c5abb6f5381ca0add7b8ba74f170612fa2773c1c
gubei <>
2024-03-13 16:14:15 +0800

    fix diffusers verison . from https://github.com/levihsu/OOTDiffusion/issues/44#issuecomment-1968365131

--- pipelines_ootd/transformer_garm_2d.py
+++ pipelines_ootd/transformer_garm_2d.py
@@ -26,7 +26,7 @@ from diffusers.configuration_utils import ConfigMixin, register_to_config
 from diffusers.models.embeddings import ImagePositionalEmbeddings
 from diffusers.utils import USE_PEFT_BACKEND, BaseOutput, deprecate
 # from diffusers.models.attention import BasicTransformerBlock
-from diffusers.models.embeddings import CaptionProjection, PatchEmbed
+from diffusers.models.embeddings import PixArtAlphaTextProjection, PatchEmbed
 from diffusers.models.lora import LoRACompatibleConv, LoRACompatibleLinear
 from diffusers.models.modeling_utils import ModelMixin
 from diffusers.models.normalization import AdaLayerNormSingle
@@ -237,7 +237,7 @@ class Transformer2DModel(ModelMixin, ConfigMixin):
 
         self.caption_projection = None
         if caption_channels is not None:
-            self.caption_projection = CaptionProjection(in_features=caption_channels, hidden_size=inner_dim)
+            self.caption_projection = PixArtAlphaTextProjection(in_features=caption_channels, hidden_size=inner_dim)
 
         self.gradient_checkpointing = False
 
